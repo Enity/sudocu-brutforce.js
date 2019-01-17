764ba3a021e366ec8d52cc39a193a42cca04a360
vlad-boroda <enity.w@gmail.com>
2019-01-17 14:32:51 +0300

    some tests

--- tests/validator.test.js
+++ tests/validator.test.js
@@ -7,24 +7,36 @@ describe('Sudocu class tests', () => {
         it('should return true if all digits is unique', () => {
             equal(Validator.validateArray([1, 2, 3, 4, 5, 6, 7, 8, 9]), true);
         });
+
         it('should return false if not unique exists', () => {
             equal(Validator.validateArray([1, 4, 3, 4, 5, 6, 7, 8, 9]), false);
         });
+
         it('should return false if two digits', () => {
             equal(Validator.validateArray([1, 2, 2, 0, 0, 0, 0, 0, 0]), false);
         });
+
         it('should return true if all digits unique but not 0', () => {
             equal(Validator.validateArray([1, 2, 0, 4, 5, 0, 7, 0, 0]), true);
+        });
+
+        it('another 1', () => {
+            equal(Validator.validateArray([1, 4, 2, 1, 0, 0, 0, 0, 0]), false);
+        });
+
+        it('another 2', () => {
+            equal(Validator.validateArray([2, 4, 3, 1, 0, 0, 0, 0, 0]), true);
+        });
+
+        it('another 3', () => {
+            equal(Validator.validateArray([9, 4, 2, 1, 0, 0, 0, 0, 9]), false);
         });
     });
 
     describe('validateSoducu method', () => {
         it('should return true if sudocu is clear', () => {
-            const res = Validator.validateSudocu(
-                new Sudocu()
-            );
+            const res = Validator.validateSudocu(new Sudocu());
             equal(res, true);
         });
-
     });
 });
