50b719da84976777a9c6d7edb62ac73676712175
vlad-boroda <enity.w@gmail.com>
2019-01-17 14:33:33 +0300

    validator improve speed

--- src/validator.js
+++ src/validator.js
@@ -3,11 +3,6 @@ export class Validator {
         return Validator.validateRows(Sudocu);
     }
 
-    static validateField(Sudocu, x, y) {
-        const row = Sudocu.getRow(x, y);
-        return Validator.validateArray(row);
-    }
-
     static validateRows(Sudocu) {
         const rows = Sudocu.getRows();
         for (let i = 0; i < rows.length; i++) {
@@ -15,14 +10,25 @@ export class Validator {
         }
         return true;
     }
+
+    static validateField(Sudocu, x, y) {
+        const { value, row } = Sudocu.getField(x, y);
+        return Validator.checkFieldIsUnique(value, row);
+    }
+
     /**
      * @param {Number[]} arr 
      */
     static validateArray(arr) {
         for (let i = 0; i < arr.length; i++) {
             if (arr[i] === 0) continue;
-            if (arr.lastIndexOf(arr[i]) !== i) return false;
+            if (!Validator.checkFieldIsUnique(arr[i], arr)) return false;
         }
         return true;
+    }
+
+    static checkFieldIsUnique(value, arr) {
+        if (arr.indexOf(value) === arr.lastIndexOf(value)) return true;
+        return false;
     }
 }
