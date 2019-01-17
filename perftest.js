1dc6dfa7a1f2fb3b110e4b91146deeffeb57adc4
vlad-boroda <enity.w@gmail.com>
2019-01-17 14:46:27 +0300

    typo in word

--- perftest.js
+++ perftest.js
@@ -2,14 +2,14 @@ const { performance } = require('perf_hooks');
 const { brut } = require('./src/brutforce');
 
 function startPerf() {
-    const TRYES = 10000;
+    const TRIES = 10000;
     const start = performance.now();
 
-    for (let i = 0; i < TRYES; i++) {
+    for (let i = 0; i < TRIES; i++) {
         brut();
     }
 
-    console.log(`finished ${TRYES} in ${performance.now() - start}`);
+    console.log(`finished ${TRIES} in ${performance.now() - start}`);
 }
 
 startPerf();
